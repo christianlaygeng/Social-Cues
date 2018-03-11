997e15a8b89f1d3625ffdc69f68b7193960a16eb
Christian Lay-Geng <claygeng@ucsd.edu>
2018-03-11 15:28:47 -0700

    added social cue

---
+++ Social Cue/Assets/Moving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+	public int movementSpeed;
+	public int rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up, Input.GetAxis ("Horizontal") * rotateSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.down, Input.GetAxis ("Horizontal") * -1 * rotateSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (0, 0, Input.GetAxis ("Vertical") * movementSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (0, 0, Input.GetAxis ("Vertical") * -1 * movementSpeed * Time.deltaTime);
+		}
+	}
+}
